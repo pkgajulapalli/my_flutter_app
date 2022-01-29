bd49d612952582f6f4e1c629c28efc00fb7d21c7
Praveen Gajulapalli <praveenkumar.gbvs@gmail.com>
2022-01-29 23:56:31 +0530

    adding large and small screen layouts

---
+++ lib/layout.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:my_flutter_app/widgets/large_screen.dart';
+import 'package:my_flutter_app/widgets/small_screen.dart';
+
+class SiteLayout extends StatelessWidget {
+  const SiteLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+      ),
+      body: SmallScreen()
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:my_flutter_app/layout.dart';
 import 'package:my_flutter_app/screens/home.dart';
 
 void main() {
@@ -13,20 +15,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Admin',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: GoogleFonts.mulishTextTheme(
+          Theme.of(context).textTheme
+        ).apply(
+          bodyColor: Colors.black
+        ),
+        pageTransitionsTheme: PageTransitionsTheme(builders: {
+          TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
+          TargetPlatform.android: FadeUpwardsPageTransitionsBuilder()
+        }),
+        primaryColor: Colors.black,
+        primarySwatch: Colors.green,
       ),
-      home: HomeScreen(),
+      home: SiteLayout(),
     );
   }
 }
